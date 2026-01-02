7e6c00eb9878f6143fb7d369844da3adaa7d03f8
7988Kunal <kunalvats7988@gmail.com>
2026-01-02 22:17:40 +0530

    Add files via upload

--- edupilot.js
+++ edupilot.js
@@ -4,11 +4,14 @@
 window.addEventListener("load",()=>{
   let preloader = document.querySelector('#preloader');
   let content = document.querySelector('#content');
+  let body = document.querySelector('body');
   setTimeout(()=>{
     preloader.classList.add('hidden');
     setTimeout(()=>{
+      body.classList.remove('loading');
       preloader.computedStyleMap.display = "none";
       content.classList.remove('hidden');
     },600)
-  },6000)
+  },1000)
 })
+
